1ae334459a76707cb7ae20941f69a14c3cb1fb4f
me-am-i <vsl_sv@bk.ru>
2021-04-01 02:51:14 +0300

    добавлен eslint, ему скормлены файлы

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
-const express = require("express");
-const getLinks = require("./src/parser2.js");
+const express = require('express');
 const cors = require('cors');
+const getLinks = require('./src/parser2.js');
 
 const app = express();
 
 app.use(cors());
 
-app.get("/", async function(req, res){
+app.get('/', async (req, res) => {
+  const { url } = req.query;
 
-    const url = req.query.url;
-
-    try {
-        const myURL = new URL(url);
-        const links = await getLinks(myURL);
-        if (links.length) {
-            res.send(links);
-        }
-        else {
-            res.send('error');
-        }
-    } catch (error) {
-        res.send('invalid');
+  try {
+    const myURL = new URL(url);
+    const links = await getLinks(myURL);
+    if (links.length) {
+      res.send(links);
+    } else {
+      res.send('error');
     }
-
+  } catch (error) {
+    res.send('invalid');
+  }
 });
 
-app.listen(3000, () => console.log(`Started...`));
+app.listen(3000, () => console.log('Started...'));
--- src/parser2.js
+++ src/parser2.js
@@ -1,22 +1,21 @@
-const jsdom = require("jsdom");
+const jsdom = require('jsdom');
+
 const { JSDOM } = jsdom;
 
-const scriptSelector = "script";
+const scriptSelector = 'script';
 const styleSelector = {
-    selector: "link",
-    type: "stylesheet"
+  selector: 'link',
+  type: 'stylesheet',
 };
 
 module.exports = async (url) => {
+  const dom = await JSDOM.fromURL(url);
+  const scriptList = [...dom.window.document.querySelectorAll(scriptSelector)];
+  const scriptLinks = scriptList.filter((item) => item.src && !item.src.includes('data:text', 0)).map((item) => item.src);
 
-    const dom = await JSDOM.fromURL(url);
-    const scriptList = [...dom.window.document.querySelectorAll(scriptSelector)];
-    const scriptLinks = scriptList.filter(item => item.src && !item.src.includes("data:text", 0)).map(item => item.src);
+  const styleList = [...dom.window.document.querySelectorAll(styleSelector.selector)];
+  const styleLinks = styleList.filter((item) => (item.rel === styleSelector.type)).map((item) => item.href);
 
-    const styleList = [...dom.window.document.querySelectorAll(styleSelector.selector)];
-    const styleLinks = styleList.filter(item => (item.rel === styleSelector.type)).map(item => item.href);
-
-    await dom.window.close();
-    return [...scriptLinks, ...styleLinks];
-
-}
+  await dom.window.close();
+  return [...scriptLinks, ...styleLinks];
+};
