82525a03ef0f31daef0c91e412e204ba75a9c640
me-am-i <vsl_sv@bk.ru>
2021-04-01 03:35:40 +0300

    удалено лишнее

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,22 +2,13 @@
   <div id="app">
     <input v-model.lazy="addr"><button v-on:click="request" v-on:keyup.enter="request">Получить список файлов</button>
     <p>Введенный адрес: {{ addr }}</p>
-
-<!--    {{ info }}-->
-
-<!--    <ol id="v-for-object"  :key="info.index">-->
-<!--      <li v-for="(value) in info">-->
-<!--         {{ value }}-->
-<!--      </li>-->
-<!--    </ol>-->
     <div v-if="(!info)">
       {{ wait }}
     </div>
     <div v-else-if="info === 'error'">
       Не удалось получить список файлов для этого адреса. Попробуйте другой.
     </div>
-    <div id="example-1" v-else-if="(info !== 'invalid')" >
-
+    <div id="list" v-else-if="(info !== 'invalid')" >
       <ul>
         <li v-for="item in info" :key="item.index" >
           {{ item }}
@@ -32,8 +23,8 @@
 
 <script>
 import axios from 'axios'
-// let url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
-let url = 'http://185.68.21.38:3000?url='
+// let host = 'http://localhost:3000?url='
+let host = 'http://185.68.21.38:3000?url='
 
 export default {
   name: 'App',
@@ -46,31 +37,15 @@ export default {
   },
   methods: {
     request: function () {
-      console.log(this.message)
       this.wait = 'Please wait...'
       this.info = ''
       axios
-        .get(url + this.addr)
+        .get(host + this.addr)
         .then(response => (this.info = response.data))
         .catch(error => console.log(error))
-
-      // this.result = this.num * this.num;
     }
   }
-  /*  data () {
-    return {
-      info: '123' // null
-    }
-  }, */
-  /* mounted () {
-    axios
-      .get(url)
-      .then(response => (this.info = response))
-      .catch(error => console.log(error))
-  } */
 }
-
-// .then(response => console.log(response))
 
 </script>
 
@@ -81,14 +56,12 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-
   width:50%;
   margin: 0 auto;
   margin-top: 60px;
 }
 
-#example-1 {
+#list {
   text-align: left;
-
 }
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -2,30 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
-// import axios from 'axios'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
   components: { App },
   template: '<App/>'
 })
-
-/*   data: {
-    message: 'hello'
-  }, */
-
-/* data () {
-    return {
-      info: null
-    }
-  },
-  mounted () {
-    axios
-      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(response => (this.message = response))
-  } */
